d77d0fb3a160e3a9846ab799efc90f11ec40a9d4
Fvtvl <fvtvlnight@gmail.com>
2023-02-14 21:49:37 +0000

    delete item from cart after payment

--- client/src/scenes/confirmation/index.jsx
+++ client/src/scenes/confirmation/index.jsx
@@ -1,7 +1,19 @@
 import { Alert, AlertTitle, Box } from '@mui/material';
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeFromCart } from '../../state';
 
 const Confirmation = () => {
+  const cart = useSelector((state) => state.cart.cart);
+  const dispatch = useDispatch();
+
+  const handleRemoveFromCart = useCallback(
+    (id) => {
+      dispatch(removeFromCart({ id }));
+    },
+    [dispatch]
+  );
+  cart.map((item) => handleRemoveFromCart(item.id));
   return (
     <Box m="90px auto" width="80%" height="50vh">
       <Alert severity="success">
